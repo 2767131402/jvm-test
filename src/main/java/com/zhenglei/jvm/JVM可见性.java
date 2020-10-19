f78abdff5f6fb774a3f171dc70a5d5eb42956756
zhenglei <zhenglei@bbdservice.com>
2020-10-19 13:47:27 +0800

    JVM

--- src/main/java/com/zhenglei/jvm/JVM可见性.java
+++ src/main/java/com/zhenglei/jvm/JVM可见性.java
@@ -1,8 +1,12 @@
 package com.zhenglei.jvm;
 
+import java.util.concurrent.TimeUnit;
 
+/**
+ * volatile 可见性 测试
+ */
 class MyData{
-    int num = 0;
+    volatile int num = 0; // 加上volatile 后 可以保证主内存值 对其他线程 可见
     public void updateNum(){
         this.num = 60;
     }
@@ -11,7 +15,18 @@ class MyData{
 public class JVM可见性 {
     public static void main(String[] args) {
         MyData myData = new MyData();
+        new Thread(()->{
+            System.out.println(Thread.currentThread().getName()+"\t come in.");
+            myData.updateNum();
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {}
+            System.out.println(Thread.currentThread().getName()+"\t update num is 60");
+        },"AAA").start();
 
-        new Thread(()—>{})
+        while (myData.num==0){
+            //主线程获取 num 的值
+        }
+        System.out.println(Thread.currentThread().getName()+"\t main num is 60");
     }
 }
